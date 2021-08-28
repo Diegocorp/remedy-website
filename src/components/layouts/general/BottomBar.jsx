8ede2c6092230c67cb1d1a8d0bf78f1322adc02c
Diegocorp <diegosandovalr.corp@gmail.com>
2021-08-27 23:53:59 -0700

    first commit in deploy repository

---
+++ src/components/layouts/general/BottomBar.jsx
@@ -0,0 +1,65 @@
+import React, { Component } from "react";
+import { Link } from "react-router-dom";
+class BottomBar extends Component {
+  render() {
+    return (
+      <div className="footer-bottom">
+        <div className="container">
+          <div className="row">
+            <div className="col-md-12">
+              <div className="copyright">
+                <p>
+                  Privacy Policy | © 2020 Remedy Insurance. All Rights Reserved
+                </p>
+              </div>
+              <ul className="menu-footer">
+                <li>
+                  <Link
+                    to="/"
+                    onClick={() => {
+                      window.location.href = "/";
+                    }}
+                    title=""
+                  >
+                    Home
+                  </Link>
+                </li>
+                <li>
+                  <Link
+                    to="/blog"
+                    onClick={() => {
+                      window.location.href = "/blog";
+                    }}
+                    title=""
+                  >
+                    My Blog
+                  </Link>
+                </li>
+                <li>
+                  <Link
+                    to="/"
+                    onClick={() => {
+                      window.location.href = "/";
+                    }}
+                    title=""
+                  >
+                    My Home
+                  </Link>
+                </li>
+                <li>
+                  <Link to="#" title="">
+                    Sample Page
+                  </Link>
+                </li>
+              </ul>
+              {/* <!-- /.menu-footer --> */}
+            </div>
+          </div>
+        </div>
+      </div>
+      /* <!-- /.footer-bottom --> */
+    );
+  }
+}
+
+export default BottomBar;
